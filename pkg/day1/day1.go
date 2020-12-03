4ec5cf7b55afb8ce564840c4c697f5a50c00953a
Abhay Soni <aymsoni29@gmail.com>
2020-12-02 22:34:57 -0800

    Init commit day 1 part 1

---
+++ pkg/day1/day1.go
@@ -0,0 +1,11 @@
+package day1
+
+import "fmt"
+
+// Part 1 of Day 1
+// Approach 1 - sort + two pointers = O(nlog(n))
+// Approach 2 - HashMap (value -> occurences) O(n)
+// Approach 3 - Two for loops (every num[i] iterate through all entries) O(n^2)
+func Part1() {
+	fmt.Println("Day 1 : Part 1")
+}
---
+++ pkg/day1/day1_test.go
@@ -0,0 +1,11 @@
+package day1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Part1(t *testing.T) {
+
+}
