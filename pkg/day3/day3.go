a8398d64fdac28d899797f820354db3e8173b658
Abhay Soni <aymsoni29@gmail.com>
2020-12-18 22:34:40 -0800

    Day 3 Part 2

--- pkg/day3/day3.go
+++ pkg/day3/day3.go
@@ -2,11 +2,12 @@ package day3
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
 
-func Part1(filename string) int {
+func Part1(filename string, right, down int) int {
 	trees := 0
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -17,20 +18,58 @@ func Part1(filename string) int {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	skip := true
 	index := 0
+	current := down
 	for scanner.Scan() {
 		line := scanner.Text()
-		if !skip {
+		if current == 0 {
+			index += right
 			if index >= len(line) {
 				index = index - len(line)
 			}
 			if string(line[index]) == "#" {
 				trees++
 			}
+			current = down
 		}
-		index += 3
-		skip = false
+		current--
 	}
 	return trees
+}
+
+type TestCaseDay3Part2 struct {
+	right int
+	down  int
+}
+
+func Part2() int {
+	cases := []TestCaseDay3Part2{
+		{
+			right: 1,
+			down:  1,
+		},
+		{
+			right: 3,
+			down:  1,
+		},
+		{
+			right: 5,
+			down:  1,
+		},
+		{
+			right: 7,
+			down:  1,
+		},
+		{
+			right: 1,
+			down:  2,
+		},
+	}
+	ans := 1
+	for i := range cases {
+		count := Part1("input.txt", cases[i].right, cases[i].down)
+		fmt.Println(count)
+		ans = ans * count
+	}
+	return ans
 }
--- pkg/day3/day3_test.go
+++ pkg/day3/day3_test.go
@@ -6,5 +6,9 @@ import (
 )
 
 func Test_Part1(t *testing.T) {
-	assert.Equal(t, 178, Part1("input.txt"))
+	assert.Equal(t, 178, Part1("input.txt", 3, 1))
+}
+
+func Test_Part2(t *testing.T) {
+	assert.Equal(t, 3492520200, Part2())
 }
